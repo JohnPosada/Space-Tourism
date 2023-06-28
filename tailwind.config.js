27a06f0c893527935101ed9a4afe507516d6b311
JohnPosada <johnposada2000@gmail.com>
2023-06-28 15:47:59 -0500

    creted project with initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#0B0D17",
+        secondary: "#D0D6F9",
+        tertiary: "#FFFFFF",
+      },
+      fontFamily: {
+        sans: ["var(--font-bellefair)"],
+        serif: ["var(--font-barlow-condensed)"],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "home-desktop": "url('/assets/home/background-home-desktop.jpg')",
       },
     },
   },
   plugins: [],
-}
+};
