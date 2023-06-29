83d294b74fdc978821525f706a71ea3e1cf50403
JohnPosada <johnposada2000@gmail.com>
2023-06-28 22:24:40 -0500

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="h-full w-full grid grid-cols-2 grid-rows-5 place-items-center">
+      <div className="row-span-full col-span-1 row-start-3">
+        <h2 className="row-span-full font-serif tracking-[5px] text-secondary text-2xl">
+          SO, YOU WANT TO TRAVEL TO
+        </h2>
+        <h1 className="font-sans text-tertiary text-[150px] font-bold">
+          SPACE
+        </h1>
+        <p className="w-[440px] font-serif text-tertiary text-lg leading-loose">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div className="row-span-full row-start-4 col-span-1 rounded-full h-64 w-64 text-3xl font-sans flex justify-center items-center text-primary bg-white">
+        EXPLORE
+      </div>
+    </div>
+  );
 }
