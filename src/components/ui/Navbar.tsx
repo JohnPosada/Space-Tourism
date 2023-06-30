2174f30dbdf395878881335529e2a908ebe981c4
JohnPosada <johnposada2000@gmail.com>
2023-06-30 01:19:56 -0500

    fixed navbar styles

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,44 +3,25 @@
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import NavbarItem from "../NavbarItem";
-import { useRouter } from "next/router";
-
-const barItems = [
-  {
-    name: "HOME",
-    number: "00",
-    href: "/",
-  },
-  {
-    name: "DESTINATION",
-    number: "01",
-    href: "/destination",
-  },
-  {
-    name: "CREW",
-    number: "02",
-    href: "/crew",
-  },
-  {
-    name: "TECHNOLOGY",
-    number: "03",
-    href: "/technology",
-  },
-];
+import { barItems } from "@/constants/navegation";
 
 const Navbar = () => {
   const path = usePathname();
+  const pathname = path === "/" ? "HOME" : path.split("/")[1].toUpperCase();
 
   return (
-    <div className="flex justify-between items-center  h-24 mt-10 ml-14">
-      {/* <div className="flex justify-center items-center h-full">
-        <hr className="w-[200px] bg-tertiary" />
-      </div> */}
-      <Image src="/assets/shared/logo.svg" alt="Logo" width={48} height={48} />
-      <hr className="relative w-1/3 left-10" />
+    <div className="flex justify-between items-center  h-24  ml-14">
+      <Image
+        src="/assets/shared/logo.svg"
+        alt="Logo"
+        width={48}
+        height={48}
+        priority
+      />
+      <hr className="relative w-1/3 left-10 opacity-50" />
       <div className="flex bg-slate-100 bg-opacity-10 backdrop-blur-sm justify-center items-center gap-12 w-[830px] h-full">
         {barItems.map((item) =>
-          path === item.href ? (
+          pathname === item.name ? (
             <NavbarItem
               key={item.name}
               name={item.name}
