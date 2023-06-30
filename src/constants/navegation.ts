e5dbaa1cfa5dc868610b9af66f40c7afbda4e797
JohnPosada <johnposada2000@gmail.com>
2023-06-30 01:13:37 -0500

    added data

---
+++ src/constants/navegation.ts
@@ -0,0 +1,60 @@
+export const barItems = [
+  {
+    name: "HOME",
+    number: "00",
+    href: "/",
+  },
+  {
+    name: "DESTINATION",
+    number: "01",
+    href: "/destination/moon",
+  },
+  {
+    name: "CREW",
+    number: "02",
+    href: "/crew/1",
+  },
+  {
+    name: "TECHNOLOGY",
+    number: "03",
+    href: "/technology",
+  },
+];
+
+export const barDestination = [
+  {
+    name: "MOON",
+    number: "01",
+    href: "/destination/moon",
+  },
+  {
+    name: "MARS",
+    number: "02",
+    href: "/destination/mars",
+  },
+  {
+    name: "EUROPA",
+    number: "03",
+    href: "/destination/europa",
+  },
+  {
+    name: "TITAN",
+    number: "04",
+    href: "/destination/titan",
+  },
+];
+
+export const slideCrew = [
+  {
+    href: "/crew/1",
+  },
+  {
+    href: "/crew/2",
+  },
+  {
+    href: "/crew/3",
+  },
+  {
+    href: "/crew/4",
+  },
+];
